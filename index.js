864d941a471c4c48e7c01671e8b62a7061eae727
nikunj bhuva <nikunjbhuva121@gmail.com>
2024-02-19 11:41:41 +0530

    hii

---
+++ index.js
@@ -0,0 +1,22 @@
+//1 que//
+
+// var units=100;
+// var perunits=3;
+// var fixed=50;
+// var total=units*perunits+fixed;
+// var subsidy=total*10/100;
+// var totalbill=total-subsidy;
+// console.log(totalbill);
+
+//2 que//
+
+var emp=300000;
+var months=emp/12;
+var homea=months*20/100;
+var travela=months*5/100;
+// var alltotal=homea-travela;
+var total=months-homea-travela;
+console.log(months);
+console.log(homea);
+console.log(travela);
+console.log(total);
